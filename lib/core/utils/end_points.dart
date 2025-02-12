6728b94c2a7a898d2ee73a75c776e454094f3f9f
Your Name <you@example.com>
2025-02-12 19:06:50 +0200

    Call api for reset code in auth service

--- lib/core/utils/end_points.dart
+++ lib/core/utils/end_points.dart
@@ -1,4 +1,5 @@
 const String baseUrl = 'https://exam.elevateegy.com/api/v1/';
 const String signinEndPoint = 'auth/signin';
 const String forgetPasswordEndPoint = 'auth/forgotPassword';
+const String verifyResetCodeEndPoint = 'auth/verifyResetCode';
 
