a568979c1bc0023b2eb35c9fd55a73cf9cbd3457
Your Name <you@example.com>
2025-02-13 04:35:59 +0200

    Finished Reset password logic and ui part 1.

--- lib/features/auth/presentation/views/verify_reset_code_view.dart
+++ lib/features/auth/presentation/views/verify_reset_code_view.dart
@@ -13,8 +13,8 @@ import 'package:online_exam/features/auth/presentation/views/reset_password_view
 import 'package:online_exam/features/auth/presentation/views/sigin_up_view.dart';
 
 class VerifyResetCodeView extends StatefulWidget {
-  const VerifyResetCodeView({super.key});
-
+  const VerifyResetCodeView({super.key, required this.email});
+final String email;
   @override
   State<VerifyResetCodeView> createState() => _VerifyResetCodeView();
 }
@@ -47,7 +47,7 @@ class _VerifyResetCodeView extends State<VerifyResetCodeView> {
               context,
               MaterialPageRoute(
                 builder: (context) =>
-                    ResetPasswordView(email: emailController.text),
+                    ResetPasswordView(email: widget.email),
               ),
             );
           }
