c79e42432f336bb45b2305696339ac61247641c0
Your Name <you@example.com>
2025-02-13 18:02:55 +0200

    Finished verify reset code logic and ui.

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -5,3 +5,4 @@ Color secondaryColor = Color(0xff7a7a7c);
 Color backgroundColor = Colors.white;
 Color greyColor = Color(0xff535353);
 Color blackColor = Color(0xff0F0F0F);
+Color pinColor = Color(0xffDFE7F7);
--- lib/features/auth/presentation/cubits/verify_code_cubit/verify_reset_code_view_model.dart
+++ lib/features/auth/presentation/cubits/verify_code_cubit/verify_reset_code_view_model.dart
@@ -2,7 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:online_exam/core/errors/failures.dart';
-import 'package:online_exam/core/utils/base_api_state.dart';
 import 'package:online_exam/features/auth/domain/repos/verify_reset_code_repo.dart';
 import 'package:online_exam/features/auth/presentation/cubits/verify_code_cubit/verify_reset_code_state.dart';
 
@@ -10,16 +9,16 @@ import 'package:online_exam/features/auth/presentation/cubits/verify_code_cubit/
 class VerifyResetCodeViewModel extends Cubit<VerifyResetCodeViewModelState> {
   VerifyResetCodeRepo verifyResetCodeRepo;
   VerifyResetCodeViewModel(this.verifyResetCodeRepo)
-      : super(VerifyResetCodeViewModelState.initial());
+      : super(VerifyResetCodeIdleState());
 
   void verifyResetCode({required String resetCode}) async {
-    emit(VerifyResetCodeViewModelState(BaseLoadingState()));
+    emit(VerifyResetCodeLoadingState());
     Either<ServerFailure, void> response =
         await verifyResetCodeRepo.verifyResetCode(resetCode: resetCode);
     response.fold((failure) {
-      emit(VerifyResetCodeViewModelState(BaseErrorState(failure.errorMessage)));
+      emit(VerifyResetCodeErrorState(failure.errorMessage));
     }, (_) {
-      emit(VerifyResetCodeViewModelState(BaseSuccessState(null)));
+      emit(VerifyResetCodeSuccessState());
     });
   }
 }
