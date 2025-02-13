2be696c003447b440f401c03cd08357507a2cc5f
Your Name <you@example.com>
2025-02-13 02:43:25 +0200

    Added reset password fun in auth service

--- lib/core/utils/end_points.dart
+++ lib/core/utils/end_points.dart
@@ -2,4 +2,4 @@ const String baseUrl = 'https://exam.elevateegy.com/api/v1/';
 const String signinEndPoint = 'auth/signin';
 const String forgetPasswordEndPoint = 'auth/forgotPassword';
 const String verifyResetCodeEndPoint = 'auth/verifyResetCode';
-
+const String resetPasswordEndPoint = 'auth/resetPassword';
