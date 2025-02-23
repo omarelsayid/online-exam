a7038e54c74a033e9f34cc8cd337421ed16111c8
omarelsayed <omarelsayd445@gmail.com>
2025-02-23 19:20:12 +0200

    finsh retrive user profile data part1

--- lib/features/user_profile/presentation/views/user_profile_view.dart
+++ lib/features/user_profile/presentation/views/user_profile_view.dart
@@ -5,6 +5,7 @@ import 'package:online_exam/core/services/di_service.dart';
 import 'package:online_exam/core/widgets/custom_app_bar.dart';
 import 'package:online_exam/features/user_profile/domain/repo/get_user_profile_repo.dart';
 import 'package:online_exam/features/user_profile/presentation/cubits/user_profile_cubit/user_profile_cubit.dart';
+import 'package:online_exam/features/user_profile/presentation/views/widgets/sigin_view_body_bloc_consumer.dart';
 import 'package:online_exam/features/user_profile/presentation/views/widgets/user_profile_view_body.dart';
 
 class UserProfileView extends StatelessWidget {
@@ -17,7 +18,7 @@ class UserProfileView extends StatelessWidget {
       child: Scaffold(
         appBar: buildCustomAppBar(
             title: 'Profile', isVisible: false, context: context),
-        body: UserProfileViewBody(), // Keep it const for performance
+        body: UserProfileViewBodyBlocConsumer(),
       ),
     );
   }
--- lib/features/user_profile/presentation/views/widgets/user_profile_view_body.dart
+++ lib/features/user_profile/presentation/views/widgets/user_profile_view_body.dart
@@ -1,44 +1,91 @@
 import 'dart:developer';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:online_exam/core/helper_function/show_error_snackbar.dart';
-import 'package:online_exam/features/user_profile/presentation/cubits/user_profile_cubit/user_profile_cubit.dart';
-import 'package:online_exam/features/user_profile/presentation/cubits/user_profile_cubit/user_profile_states.dart';
 
-class UserProfileViewBody extends StatelessWidget {
-  const UserProfileViewBody({super.key});
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:online_exam/core/utils/constans.dart';
+import 'package:online_exam/features/user_profile/domain/entities/user_profile_entity.dart';
+
+class UserProfileViewBody extends StatefulWidget {
+  const UserProfileViewBody({super.key, required this.userProfileEntity});
+  final UserProfileEntity userProfileEntity;
+
+  @override
+  State<UserProfileViewBody> createState() => _UserProfileViewBodyState();
+}
+
+class _UserProfileViewBodyState extends State<UserProfileViewBody> {
+  late final TextEditingController _usernameController =
+      TextEditingController(text: widget.userProfileEntity.username);
+  late final TextEditingController _firstNameController =
+      TextEditingController(text: widget.userProfileEntity.firstName);
+  late final TextEditingController _lastNameController =
+      TextEditingController(text: widget.userProfileEntity.lastName);
+  late final TextEditingController _emailController =
+      TextEditingController(text: widget.userProfileEntity.email);
+  late final TextEditingController _phoneController =
+      TextEditingController(text: widget.userProfileEntity.phone);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _phoneController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<UserProfileCubit, UserProfileStates>(
-      builder: (context, state) {
-        if (state is UserProfileLoading) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (state is UserProfileLoaded) {
-          return Center(
-              child: Text(state.userProfileEntity.firstName.toString()));
-        } else if (state is UserProfileInitial) {
-          return ElevatedButton(
-            child: const Text('Get User Profile'),
-            onPressed: () {
-              context
-                  .read<UserProfileCubit>()
-                  .getUserProfile(); // Use context.read instead of getIt
-            },
-          );
-        } else if (state is UserProfileFailure) {
-          return Center(child: Text(state.errorMessage));
-        } else {
-          return const Center(child: Text('Something went wrong'));
-        }
-      },
-      listener: (context, state) async {
-        if (state is UserProfileLoaded) {
-          log(state.userProfileEntity.toString());
-        } else if (state is UserProfileFailure) {
-          ShowSnackbar(state.errorMessage, context);
-        }
-      },
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding.w),
+      child: Column(
+        children: [
+          SizedBox(height: 20.h),
+          TextFormField(
+            controller: _usernameController, // ✅ Assigned controller
+            decoration: InputDecoration(labelText: 'User Name'),
+          ),
+          SizedBox(height: 24.h),
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: _firstNameController,
+                  decoration: InputDecoration(labelText: 'First Name'),
+                ),
+              ),
+              SizedBox(width: 17.w),
+              Expanded(
+                child: TextFormField(
+                  controller: _lastNameController,
+                  decoration: InputDecoration(labelText: 'Last Name'),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 24.h),
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(labelText: 'Email'),
+          ),
+          SizedBox(height: 24.h),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(labelText: 'Password'),
+          ),
+          SizedBox(height: 24.h),
+          TextFormField(
+            controller: _phoneController,
+            decoration: InputDecoration(labelText: 'Phone Number'),
+          ),
+        ],
+      ),
     );
   }
 }
