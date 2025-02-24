b70afe5e6252119b9868c16cfd6f4281c2811538
ALHassan Musleh <80463251+ALHassanMusleh@users.noreply.github.com>
2025-02-24 05:01:25 +0200

    Added Update profile cubit and states.

---
+++ lib/features/user_profile/presentation/cubits/update_profile_cubit/update_profile_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:online_exam/core/errors/failures.dart';
+import 'package:online_exam/features/user_profile/domain/repo/update_profile_repo.dart';
+import 'package:online_exam/features/user_profile/presentation/cubits/update_profile_cubit/update_profile_state.dart';
+
+class UpdateProfileCubit extends Cubit<UpdateProfileState> {
+  UpdateProfileRepo updateProfileRepo;
+
+  UpdateProfileCubit(this.updateProfileRepo) : super(UpdateProfileIdleState());
+
+  void updateProfile({
+    String? username,
+    String? firstName,
+    String? lastName,
+    String? email,
+    String? phone,
+  }) async {
+    emit(UpdateProfileLoadingState());
+    Either<ServerFailure, void> response =
+        await updateProfileRepo.updateProfile(
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      phone: phone,
+      username: username,
+    );
+    response.fold((failure) {
+      emit(UpdateProfileErrorState(failure.errorMessage));
+    }, (_) {
+      emit(UpdateProfileSuccessState());
+    });
+  }
+}
