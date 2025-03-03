06c9c3f8b16f918991d59945d6383b8057c2aae6
ALHassan Musleh <80463251+ALHassanMusleh@users.noreply.github.com>
2025-03-03 07:09:54 +0200

    Added All exams on subjects cubit and states.

---
+++ lib/features/exam/presentation/cubits/get_all_exams_on_subjects_cubit/get_all_exams_on_subjects_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:online_exam/features/exam/domain/repo/get_all_exams_on_subject_repo.dart';
+import 'package:online_exam/features/exam/presentation/cubits/get_all_exams_on_subjects_cubit/get_all_exams_on_subjects_state.dart';
+
+class GetAllExamsOnSubjectsCubit extends Cubit<GetAllExamsOnSubjectsState> {
+  final GetAllExamsOnSubjectRepo getAllExamsOnSubjectRepo;
+  GetAllExamsOnSubjectsCubit(this.getAllExamsOnSubjectRepo)
+      : super(GetAllExamsOnSubjectsIdleState());
+
+  void getAllExamsOnSubject({required String subjectId}) async {
+    emit(GetAllExamsOnSubjectsLoadingState());
+    final response = await getAllExamsOnSubjectRepo.getAllExamsOnSubject(
+        subjectId: subjectId);
+    response.fold(
+      (failure) {
+        emit(GetAllExamsOnSubjectsErrorState(failure.errorMessage));
+      },
+      (exams) {
+        emit(GetAllExamsOnSubjectsSuccessState(exams));
+      },
+    );
+  }
+}
